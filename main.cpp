250635e3b1c91e7bd5eb5813c7430939f799873f
cturco <cturco@umich.edu>
2020-01-18 18:47:18 -0500

    updates to stats.cpp, did not finish function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include "stats.h"
 #include "p1_library.h"
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cmath>
+#include <iomanip>
+#include <limits>
 using namespace std;
 
 int main() {
--- stats.cpp
+++ stats.cpp
@@ -2,10 +2,42 @@
 #include "stats.h"
 #include <cassert>
 #include <vector>
+
+//added
+#include <cmath>
+#include <iomanip>
+#include <limits>
+#include <string>
+
 using namespace std;
 
 vector<vector<double> > summarize(vector<double> v) {
-  assert(false);
+    //outer vector that will be returned by the function
+    vector<double> outer;
+    
+    double smallest = v[0];
+    const double epsilon = 0.00001;
+    int count_of_value = 0;
+    
+    //finds the smallest value in the vector v.
+    for (int i = 0; i < v.size(); ++i) {
+        if (v[i] < smallest) {
+            smallest = v[i];
+        }
+    }
+    
+    
+    for (int i = 0; i < v.size(); ++i) {
+        
+        /* if values are equal (must use epsilon because double comparison */
+        if (abs(smallest - v[i]) < epsilon) {
+            ++count_of_value;
+        }
+        
+        
+    }
+    
+    assert(false);
 }
 
 int count(vector<double> v) {
